7c2719ff02e48888378c7959926c5965baf0d69d
dlwhddksiq <dlwhddksiq@naver.com>
2022-07-26 18:27:12 +0900

    modify

---
+++ src/test/java/com/Goorm/youtube/Youtubeclone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Goorm.youtube.Youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
